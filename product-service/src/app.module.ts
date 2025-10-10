9ef6c9ed16cb83d45ce24c099de36fa1987606ee
TheJudyEssam <ju121.farouk@gmail.com>
2025-10-10 14:30:45 +0300

    [FEATURE-1] added products schema

--- product-service/src/app.module.ts
+++ product-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ MongooseModule.forRoot('mongodb+srv://ju:<db_password>@cluster0.8qbioua.mongodb.net/')],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ product-service/src/products/models/products.schema.ts
@@ -0,0 +1,25 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Double } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ required: true })
+  product_name: string;
+
+  @Prop()
+  product_description: string;
+
+  @Prop({ required: true })
+  product_price: number;
+
+  @Prop({ required: true })
+  product_image: string;
+
+  @Prop({ required: true })
+  stock_quantity: number
+
+  @Prop({ required: true })
+  product_category: number
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
